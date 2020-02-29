ada2f3745f76c3c4ca68c2d8b55a39b2bba34895
Boris Melikian <boris@lariushin.org>
2020-02-29 09:37:33 +0200

    fix eslint-loader issue and fix yarn check.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -10,13 +10,16 @@ function resolve (dir) {
 
 const createLintingRule = () => ({
   test: /\.(js|vue)$/,
-  loader: 'eslint-loader',
   enforce: 'pre',
   include: [resolve('src'), resolve('test')],
-  options: {
-    formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
-  }
+  use: [
+    {
+      loader: 'eslint-loader',
+      options: {
+        formatter: require('eslint/lib/formatters/stylish')
+      },
+    },
+  ],
 })
 
 module.exports = {
